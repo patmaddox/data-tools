9bf31935ccdae4eab5793f89eb8e3a69646c5d49
Pat Maddox <pat@patmaddox.com>
2023-07-20 15:47:47 -0700

    back up from a snapshot

--- backup.sh
+++ backup.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-rsync -a ./pending-local/ nas:/nas/pending/$(hostname -s)
+set -e -o pipefail
+snapshot=$(tmutil localsnapshot | grep -o '\d\d\d\d-\d\d-\d\d-\d\d\d\d\d\d')
+mkdir -p /tmp/$snapshot
+mount_apfs -s com.apple.TimeMachine.${snapshot}.local /System/Volumes/Data /tmp/$snapshot
+rsync -a /tmp/${snapshot}${HOME}/data/pending-local/ nas:/nas/pending/$(hostname -s)
+umount /tmp/$snapshot
+rmdir /tmp/$snapshot
