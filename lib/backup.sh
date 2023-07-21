03ab74239573980fd006aef6bac50632e077bf5c
Pat Maddox <pat@patmaddox.com>
2023-07-20 18:10:36 -0700

    rsync delete files

--- lib/backup.sh
+++ lib/backup.sh
@@ -4,7 +4,7 @@ echo "BEG: $(date)" >> backup.log
 snapshot=$(tmutil localsnapshot | grep -o '\d\d\d\d-\d\d-\d\d-\d\d\d\d\d\d')
 mkdir -p /tmp/$snapshot
 mount_apfs -s com.apple.TimeMachine.${snapshot}.local /System/Volumes/Data /tmp/$snapshot > /dev/null 2>&1
-rsync -a /tmp/${snapshot}${HOME}/data/pending-local/ nas:/nas/pending/$(hostname -s)
+rsync -a --delete /tmp/${snapshot}${HOME}/data/pending-local/ nas:/nas/pending/$(hostname -s)
 umount /tmp/$snapshot
 rmdir /tmp/$snapshot
 ssh nas "TZ=UTC sudo sanoid --take-snapshots --quiet"
