c7eb8984d047e2a1a640d7a7b4f8c675a58dd47f
Pat Maddox <pat@patmaddox.com>
2023-08-17 17:23:37 -0700

    extract stuff to .pending-tools dir

--- .pending-tools/lib/backup.sh
+++ .pending-tools/lib/backup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e -o pipefail
-echo "BEG: $(date)" >> backup.log
+echo "BEG: $(date)" >> .pending-tools/backup.log
 snapshot=$(tmutil localsnapshot | grep -o '\d\d\d\d-\d\d-\d\d-\d\d\d\d\d\d')
 mkdir -p /tmp/$snapshot
 mount_apfs -s com.apple.TimeMachine.${snapshot}.local /System/Volumes/Data /tmp/$snapshot > /dev/null 2>&1
-rsync -a --delete --exclude-from=.rsync-ignore /tmp/${snapshot}${HOME}/pending/ nas:/nas/pending/$(hostname -s)/
+rsync -a --delete --exclude-from=.pending-tools/rsync-ignore /tmp/${snapshot}${HOME}/pending/ nas:/nas/pending/$(hostname -s)/
 umount /tmp/$snapshot
 rmdir /tmp/$snapshot
 tmutil deletelocalsnapshots $snapshot > /dev/null
--- Justfile
+++ Justfile
@@ -2,11 +2,11 @@ help:
   just -l
 
 @backup:
-  ./lib/backup.sh
+  ./.pending-tools/lib/backup.sh
 
 @log:
   echo "backup.log:"
-  tail -n 4 backup.log
+  tail -n 4 .pending-tools/backup.log
   echo "canary.log:"
   ssh nas 'tail /nas/canary/canary.log && echo "now: $(TZ=UTC date)"'
 
